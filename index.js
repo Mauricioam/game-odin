2aeb50d1f25b56bfa453ed98b37d828cc8449577
Mauricioam <mauricioam91@gmail.com>
2025-10-27 21:58:06 -0300

    changed playRound logic, to adapt to the eventhandler

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ const siccor = document.querySelector("#siccor");
 
 const container = document.querySelector("#container");
 
-console.log(container);
-
-container.addEventListener("click", (event) => {
-  event.preventDefault();
-  console.log(event.target.value);
-});
+container.addEventListener("click", playRound);
 
 function getRandom(min, max) {
   return Math.round(Math.random() * (max - min) + min);
@@ -17,44 +12,44 @@ function getRandom(min, max) {
 
 function getComputerChoice() {
   let randomNum = getRandom(1, 3);
-  if (randomNum == 1) return paper;
-  else if (randomNum == 2) return rock;
-  else return siccor;
-}
-
-function getHumanChoice() {
-  const userInput = prompt("Mensaje");
-  return userInput.toLowerCase();
+  if (randomNum == 1) return "paper";
+  else if (randomNum == 2) return "rock";
+  else return "siccor";
 }
 
 //const userSelection = getHumanChoice(); Solo llama una vez y queda un valor asignado a la variable
 // asi no se puede volver a llamar a la funcion
 
 //const computerSelection = getComputerChoice();
+let humanScore = 0;
+let computerScore = 0;
+function playRound(event) {
+  console.log(humanScore);
 
+  event.preventDefault();
+  let humanOption = event.target.value;
+  let computerOption = getComputerChoice();
+  console.log(computerOption, humanOption);
+  if (humanOption == computerOption) return console.log("Tie!");
+  if (humanOption == "paper" && computerOption == "rock") {
+    humanScore++;
+    return console.log("Paper beats rock!");
+  }
+  if (humanOption == "rock" && computerOption == "siccor") {
+    humanScore++;
+    return console.log("Rock crushed the siccor");
+  }
+  if (humanOption == "siccor" && computerOption == "paper") {
+    humanScore++;
+    return console.log("OMG siccor murdered the paper");
+  } else {
+    computerScore++;
+    return console.log("Computer wins");
+  }
+}
 function playGame() {
   let humanScore = 0;
   let computerScore = 0;
-  function playRound(humanChoice, computerChoice) {
-    let humanOption = humanChoice();
-    let computerOption = computerChoice();
-    if (humanOption == computerOption) return console.log("Tie!");
-    if (humanOption == paper && computerOption == rock) {
-      humanScore++;
-      return console.log("Paper beats rock!");
-    }
-    if (humanOption == rock && computerOption == siccor) {
-      humanScore++;
-      return console.log("Rock crushed the siccor");
-    }
-    if (humanOption == siccor && computerOption == paper) {
-      humanScore++;
-      return console.log("OMG siccor murdered the paper");
-    } else {
-      computerScore++;
-      return console.log("Computer wins");
-    }
-  }
 
   // while (humanScore < 5 && computerScore < 5) {
   //   console.log("Tus puntos:" + " " + humanScore);
